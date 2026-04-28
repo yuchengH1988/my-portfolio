32cbbdca1925381951155ca8a3bb643d68dfe42e
CalvinHuang <yucheng.h.1988@gmail.com>
2026-04-28 20:05:35 +0800

    fix linkin

--- components/Layout/Contact.vue
+++ components/Layout/Contact.vue
@@ -1,7 +1,7 @@
 <script setup>
 const links = ref([
   { name: 'Email', url: 'mailto:calvin.huang.dev@gmail.com' },
-  { name: 'LinkedIn', url: 'https://www.linkedin.com/in/huangyucheng/' },
+  { name: 'LinkedIn', url: 'https://www.linkedin.com/in/yu-cheng-huang-870585138/' },
   { name: 'GitHub', url: 'https://github.com/yuchengH1988' },
   { name: 'Open Process', url: 'https://openprocessing.org/user/436847/#sketches' }
 ])
